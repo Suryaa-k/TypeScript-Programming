a376b6b98532c3bb0719da27ce8decebe6c5a108
Suryaa-k <suryaanish1149198@gmail.com>
2026-07-22 14:58:24 +0530

    Flow Control

---
+++ Conditional Stmts.ts
@@ -0,0 +1,19 @@
+let score: number = 82;
+
+if (score >= 90) {
+    console.log("Grade: A");
+} else if (score >= 75) {
+    console.log("Grade: B");
+} else if (score >= 50) {
+    console.log("Grade: C");
+} else {
+    console.log("Grade: F");
+}
+
+let day: number = 3;
+switch (day) {
+    case 1: console.log("Monday"); break;
+    case 2: console.log("Tuesday"); break;
+    case 3: console.log("Wednesday"); break;
+    default: console.log("Invalid day");
+}
---
+++ Looping Stmts.ts
@@ -0,0 +1,18 @@
+// For loop
+for (let i: number = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+// While loop
+let j: number = 1;
+while (j <= 5) {
+    console.log(j);
+    j++;
+}
+
+// Do-while loop
+let k: number = 1;
+do {
+    console.log(k);
+    k++;
+} while (k <= 5);
